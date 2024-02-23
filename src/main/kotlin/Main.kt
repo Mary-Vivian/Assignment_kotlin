14ad818855a697d0e3b523586974413488ae98f7
Mary-Vivian <muthonimaryvivian>
2024-02-23 07:36:03 +0300

    added file

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,23 @@
+fun main() {
+    enterName("Hello everyone")
+    addTwoNumbers(25,7)
+    sumOfNumbers(6,8,5,9)
+    printOfInterest("My interests are discovering new things and travelling")
+
+}
+fun enterName(name:String){
+    println(name)
+
+}
+fun addTwoNumbers(num1:Int,num2:Int){
+     var result=num1%num2
+    println(result)
+}
+fun sumOfNumbers(numb1:Int,numb2:Int,numb3:Int,numb4:Int){
+    var sum=numb1+numb2+numb3+numb4
+    println(sum)
+}
+fun printOfInterest(Interest:String){
+    println(Interest)
+
+}
